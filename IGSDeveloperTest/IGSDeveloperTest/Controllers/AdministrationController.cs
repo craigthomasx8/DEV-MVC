1e00dfcced8bc11e3546d18d54b4de8654ae120d
Craig Thomas <craig.thomas@outlook.com>
2016-11-11 23:52:37 +0000

    MVC.Grid added to Administration View

---
+++ IGSDeveloperTest/IGSDeveloperTest/Controllers/AdministrationController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IGSDeveloperTest.Models;
+using IGSDeveloperTest.Datamodel;
+
+namespace IGSDeveloperTest.Controllers
+{
+    public class AdministrationController : Controller
+    {
+        // GET: Administration
+        public ActionResult Index()
+        {
+            var vm = new AdministrationViewModels();
+            var en = new IGSEntities();
+
+            vm.CompanyList = (from x in en.Companies select x).ToList();
+            vm.GenderList = (from x in en.Genders select x).ToList();
+            vm.TitleList = (from x in en.Titles select x).ToList();
+
+            return View(vm);
+        }
+    }
+}
---
+++ IGSDeveloperTest/IGSDeveloperTest/Models/AdministrationViewModels.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using IGSDeveloperTest.Datamodel;
+
+namespace IGSDeveloperTest.Models
+{
+    public class AdministrationViewModels
+    {
+        public AdministrationViewModels()
+        {
+            this.CompanyList = new List<Company>();
+            this.GenderList = new List<Gender>();
+            this.TitleList = new List<Title>();
+        }
+
+        public List<Company> CompanyList { get; set; }
+        public List<Gender> GenderList { get; set; }
+        public List<Title> TitleList { get; set; }
+    }
+}
